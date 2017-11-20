14dd6a5cae019213b2ac476ab667352cc5ae140e
Jiří Soběslavský <sobesjir@fel.cvut.cz>
2017-11-20 16:54:24 +0100

    first simple IElerning process

---
+++ src/main/java/org/camunda/bpm/Elerning/ElerningProcessApplication.java
@@ -0,0 +1,7 @@
+package org.camunda.bpm.Elerning;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+@ProcessApplication("Elearning Process")
+public class ElerningProcessApplication extends ServletProcessApplication {
+}
