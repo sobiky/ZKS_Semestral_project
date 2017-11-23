c2514887775d8f441fd44c4bb80f6e5d441d6722
Jiří Soběslavský <sobesjir@fel.cvut.cz>
2017-11-23 11:35:09 +0100

    create new form and added new pool manager and his task

--- src/main/java/org/camunda/bpm/Elerning/EvaluationTestService.java
+++ src/main/java/org/camunda/bpm/Elerning/EvaluationTestService.java
@@ -1,25 +1,34 @@
 package org.camunda.bpm.Elerning;
 
+import org.camunda.bpm.BpmPlatform;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class EvaluationTestService implements JavaDelegate {
-    private boolean[] arraySuccessKeys;
     private final static Logger LOGGER = Logger.getLogger("EvaluationTest");
 
     public void execute(DelegateExecution delegateExecution) throws Exception {
-
         //tohle je klic pro uspeny pruchod
-        arraySuccessKeys = new boolean[]{true, false, false};
-        LOGGER.info("KEYS"+arraySuccessKeys+"\n");
+        boolean[] arraySuccessKeys = new boolean[]{true, false, false};
+        LOGGER.info("KEYS"+ Arrays.toString(arraySuccessKeys) +"\n");
         evaulate(arraySuccessKeys,delegateExecution);
 //        delegateExecution.setVariable("result", Boolean.TRUE);
         LOGGER.info("\n" + "test1 > " + delegateExecution.getVariable("otazka01") + "\n" +
                 "test2 > " + delegateExecution.getVariable("otazka02") + "\n" +
                 "test3 > " + delegateExecution.getVariable("otazka03") + "\n" +
                 "result > " + delegateExecution.getVariable("result") + "\n");
+//        LOGGER.info(delegateExecution.getProcessInstanceId());
+//todo asi good cesta k moznosti vytvoreni tasku
+        //        DelegateExecution delegate = delegateExecution.getProcessInstance();
+//        delegate.setVariable("requestor","demo");
+//        ProcessEngine processEngine = BpmPlatform.getProcessEngineService().getDefaultProcessEngine();
+//        processEngine.getRuntimeService()
+//  LOGGER.info();
     }
 
     private void evaulate(boolean[] booleans, DelegateExecution delegate) {
